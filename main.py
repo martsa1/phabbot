0c936d438f2689783a951b4e03afc6c4b7ef8882
Sam Martin <Sam.Martin@Net2Edge.com>
2017-10-17 23:24:40 +0100

    beginnings of a phabbot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import discord
 import asyncio
-
+import re
 
 client = discord.Client()
 
@@ -12,6 +12,25 @@ async def on_ready():
     print(client.user.id)
     print('------')
 
+
+def check_phab_object (message):
+    ''' Checks if the user mentioned a !task
+    '''
+    task_regex = re.compile(r'!(T\d+)')
+    diff_regex = re.compile(r'!(D\d+)')
+    # repo_regex = re.compile(r'!(r\d+)')
+    # commit_regex = re.compile(r'!(r\d+)')
+    task_result = task_regex.search(str(message))
+    diff_result = diff_regex.search(str(message))
+    response = ''
+    if task_result:
+        response = 'You mentioned a task!\n{} to be precise!'.format(task_result.group(1))
+    if diff_result:
+        response += 'You mentioned a task!\n{} to be precise!'.format(diff_result.group(1))
+
+    if response == '':
+        return None
+    return response
 
 @client.event
 async def on_message(message):
@@ -27,7 +46,9 @@ async def on_message(message):
         await asyncio.sleep(5)
         await client.send_message(message.channel, 'Done sleeping')
 
-    elif '!T123' in message.content:
-        await client.send_message(message.channel, 'You mentioned a task!  T123 to be precise!')
+    print('Checking for a task in: {}'.format(message.content))
+    task = check_phab_object(message.content)
+    if task is not None:
+        await client.send_message(message.channel, task)
 
 client.run('MzY5OTQ5MTM2MjM5MzI5Mjgz.DMgC5A.MrxR71mAAiYIRDZgjF5y7fztZRU')
